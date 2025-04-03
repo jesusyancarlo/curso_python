b88f977b3a0070e7ad2e012e98d16bdd619cc89b
jesusyancarlo <jesusyancarlo2005@gmail.com>
2025-04-03 11:03:53 -0700

    Agregué argumentos de línea de commando a app.py

--- linea/app.py
+++ linea/app.py
@@ -7,23 +7,15 @@ def main(m: float, b: float):
     Función principal que calcula las coordenadas de una línea recta
     Recibimos m y b
     Regresa: nada'''
-    # m = 2.0
-    # b = 3.0
-    # X = [x for x in range(1,11)]
-    # Y = [funciones.calcular_y(x, m, b) for x in X]
-    # print("Enteros:")
-    # coordenadas_enteros = list(zip(X,Y))
-    # print(coordenadas_enteros)
-    XF = [x / 10.0 for x in range(10, 110, 5)]
-    YF = [funciones.calcular_y(x, m, b) for x in XF]
-    coordenadas_flotantes = list(zip(XF, YF))
-    print("Flotantes:")
-    print(coordenadas_flotantes)
+    X = [x/10.0 for x in range(10,110,5)]
+    Y = [funciones.calcular_y(x, m, b) for x in X]
+    coordenadas= list(zip(X,Y))
+    print(coordenadas)
 
 if __name__ == '__main__':
-     parser = argparse.ArgumentParser()
+     parser = argparse.ArgumentParser(description='Calcula las coordenadas de una línea recta')
      parser.add_argument('-m', type=float, help='Pendiente de la línea', default=2.0)
-     parser.add_argument('-b', type=float, help='Ordenada al origen')
+     parser.add_argument('-b', type=float, help='Intersección en y', default=3.0)
      args = parser.parse_args()
      main(args.m, args.b)
      #main(m=2.0, b=3.0)
--- linea/funciones.py
+++ linea/funciones.py
@@ -1,4 +1,5 @@
 # archivo con todas las funciones necesarias para la aplicación "linea"
+
 def calcular_y(x:float, m:float, b:float)->float:
     '''
     Calcula el valor de y en una línea recta
@@ -9,14 +10,12 @@ def calcular_y(x:float, m:float, b:float)->float:
     '''
     return (m * x) + b
 
-
 def test_linea():
     '''
     Comprobamos calcular_y()
     '''
     y  = calcular_y(0.0, 2.0, 3.0)
     return y
-
 
 if __name__ == '__main__':
     if test_linea() == 3.0:
