3cd912cf2de7e83aa17e802b80a5a7a6678f8df5
jesusyancarlo <jesusyancarlo2005@gmail.com>
2025-04-03 09:53:23 -0700

    Modifiqué calcular_y() para aceptar solo flotantes

--- linea/funciones.py
+++ linea/funciones.py
@@ -1,5 +1,5 @@
 # archivo con todas las funciones necesarias para la aplicación "linea"
-def calcular_y(x, m, b):
+def calcular_y(x:float, m:float, b:float)->float:
     '''
     Calcula el valor de y en una línea recta
     x: valor de x
@@ -14,12 +14,12 @@ def test_linea():
     '''
     Comprobamos calcular_y()
     '''
-    y = calcular_y(0, 2, 3)
+    y  = calcular_y(0.0, 2.0, 3.0)
     return y
 
 
 if __name__ == '__main__':
-    if test_linea() == 3:
+    if test_linea() == 3.0:
         print('Test exitoso')
     else:
         print('Test fallido')
