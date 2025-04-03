425f70eb706ccd5c2698d58cd6c195a478e11452
jesusyancarlo <jesusyancarlo2005@gmail.com>
2025-04-03 01:20:56 -0700

    Agregué archivo de funciones.py

--- linea/app.py
+++ linea/app.py
@@ -1,22 +1,12 @@
 # cálculo de coordenadas de líneas
-def calcular_y(x, m, b):
-    '''
-    Calcula el valor de y en una línea recta
-    x: valor de x
-    m: pendiente
-    b: intersección en y
-    regresa el valor de y
-    '''
-    return m * x + b
-
+import funciones
 
 def main():
     m = 2
     b = 3
     x = 5
-    y = calcular_y(x, m, b)
+    y = funciones.calcular_y(x, m, b)
     print(f'Para x = {x}, y = {y}')
-
 
 if __name__ == '__main__':
     main()
---
+++ linea/funciones.py
@@ -0,0 +1,25 @@
+# archivo con todas las funciones necesarias para la aplicación "linea"
+def calcular_y(x, m, b):
+    '''
+    Calcula el valor de y en una línea recta
+    x: valor de x
+    m: pendiente
+    b: intersección en y
+    regresa el valor de y
+    '''
+    return (m * x) + b
+
+
+def test_linea():
+    '''
+    Comprobamos calcular_y()
+    '''
+    y = calcular_y(0, 2, 3)
+    return y
+
+
+if __name__ == '__main__':
+    if test_linea() == 3:
+        print('Test exitoso')
+    else:
+        print('Test fallido')
