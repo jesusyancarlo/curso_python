1013e224c0dabee6b46b06a185cbbd67496125c2
jesusyancarlo <jesusyancarlo2005@gmail.com>
2025-04-04 09:36:24 -0700

    movieDatabase: Login ya funciona

--- movieDatabase/app.py
+++ movieDatabase/app.py
@@ -1,16 +1,17 @@
 ''' Programa principal de movieDatabase '''
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 import random
 import os
 import movie_clases as mc
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24) # Clave secreta para sesiones
 sistema = mc.SistemaCine()
 ruta = 'datos/movies_db - '
 actores_csv = ruta + 'actores.csv'
 peliculas_csv = ruta + 'peliculas.csv'
 relaciones_csv = ruta + 'relacion.csv'
-users_csv = ruta + 'users.csv'
+users_csv = ruta + 'users_hashed.csv'
 sistema.cargar_csv(actores_csv,mc.Actor)
 sistema.cargar_csv(peliculas_csv,mc.Pelicula)
 sistema.cargar_csv(relaciones_csv,mc.Relacion)
@@ -45,6 +46,31 @@ def pelicula(id_pelicula):
     pelicula = sistema.peliculas[id_pelicula]
     actores = sistema.obtener_personajes_por_pelicula(id_pelicula)
     return render_template('pelicula.html', pelicula=pelicula, lista_actores=actores)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        exito  = sistema.login(username, password)
+        if exito:
+            session['logged_in'] = True
+            session['username'] = sistema.usuario_actual.nombre_completo
+            flash('Inicio de sesión exitoso', 'success')
+            return redirect(url_for('index'))
+            #return render_template('index.html')
+        else:
+            flash('Usuario o contraseña incorrectos', 'danger')
+            return render_template('login.html')
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    ''' Cierra la sesión del usuario '''
+    session.clear()
+    sistema.usuario_actual = None
+    flash('Has cerrado sesión correctamente', 'info')
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
