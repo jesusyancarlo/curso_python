cc6378f38d7446319ab2829e8ae4ad458e0fe97c
jesusyancarlo <jesusyancarlo2005@gmail.com>
2025-04-03 19:33:54 -0700

    carga y despliega plantillas, así como carga lista de palabras de un archivo de texto

--- ahorcado/funciones.py
+++ ahorcado/funciones.py
@@ -6,11 +6,43 @@ def carga_archivo_texto(archivo: str) -> list:
     '''
     Carga un archivo de texto y devuelve una lista con las oraciones del archivo
     '''
-    with open(archivo, 'r') as file:
+    with open(archivo, 'r',encoding='utf-8') as file:
         oraciones = file.readlines()
     return oraciones
 
+
+def carga_plantillas(nombre_plantilla: str) -> dict:
+    '''
+    Carga las plantillas del juego apartir de un archivo de texto
+    '''
+    plantillas = {}
+    for i in range(6):
+        plantillas[i] = carga_archivo_texto(f'./plantillas/{nombre_plantilla}-{i}.txt')
+    return plantillas
+
+def despliega_plantilla(diccionario: dict, nivel: int):
+    '''
+    Despliega una plantilla del juego
+    '''
+    if nivel in diccionario:
+        template = diccionario[nivel]
+        for renglon in template:
+            print(renglon)
+
+def obten_palabras(lista: list) -> list:
+    '''
+    Obtiene las palabras de un texto
+    '''
+    texto = ' '.join(lista[120:])
+    palabras = texto.split()
+    minusculas = [palabra.lower() for palabra in palabras]
+    set_palabras = set(minusculas)
+    return list(set_palabras)
+
+
 if __name__ == '__main__':
-    lista = carga_archivo_texto('./plantillas/plantilla-0.txt')
-    for elemento in lista:
-        print(elemento)
+     plantillas = carga_plantillas('plantilla')
+     despliega_plantilla(plantillas, 5)
+     lista_oraciones = carga_archivo_texto('./datos/pg15532.txt')
+     lista_palabras = obten_palabras(lista_oraciones)
+     print(lista_palabras[:50])
