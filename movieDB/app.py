9bf5888daf57b6d7760f07c750724b115d8e52a9
jesusyancarlo <jesusyancarlo2005@gmail.com>
2025-04-04 09:43:07 -0700

    movieDB: Ya funciona login

--- movieDB/app.py
+++ movieDB/app.py
@@ -5,6 +5,7 @@ import random
 import movie_classes as mc
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)  # Clave secreta para la sesión
 sistema = mc.SistemaCine()
 archivo_actores    = "datos/movies_db - actores.csv"
 archivo_peliculas  = "datos/movies_db - peliculas.csv"
@@ -39,5 +40,20 @@ def actor(id_actor):
     personajes = sistema.obtener_personajes_por_estrella(id_actor)
     return render_template('actor.html', actor=actor, lista_peliculas=personajes)
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    ''' Muestra el formulario de login '''
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        exito = sistema.login(username, password)
+        if exito:
+            session['logged_in'] = True
+            session['username'] = sistema.usuario_actual.nombre_completo
+            return redirect(url_for('index'))
+        else:
+            error = 'Usuario o contraseña incorrectos'
+            return render_template('login.html'))
+    return render_template('login.html')
 if __name__ == '__main__':
     app.run(debug=True)
