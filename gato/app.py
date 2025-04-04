301609bb175c855a745b18410985f4a39a648e56
jesusyancarlo <jesusyancarlo2005@gmail.com>
2025-04-03 19:13:19 -0700

    Pregunta al usuario si desa continuar

--- gato/app.py
+++ gato/app.py
@@ -15,6 +15,9 @@ def main():
         g = tablero.juego(dsimbolos)
         tablero.actualiza_score(score,g)
         tablero.despliega_tablero(score)
+        seguir = input('¿Quieres seguir? (s/n): ')
+        if seguir.lower() == 'n':
+             corriendo = False
 
 if __name__ == '__main__':
     main()
