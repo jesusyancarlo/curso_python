50f05e406e0b0cc88f329638e9cc4a22b05f5732
jesusyancarlo <jesusyancarlo2005@gmail.com>
2025-04-03 19:05:21 -0700

    Se debía eliminar el if g is not None

--- gato/app.py
+++ gato/app.py
@@ -13,21 +13,6 @@ def main():
     while corriendo:
         dsimbolos = {x:x for x in numeros}
         g = tablero.juego(dsimbolos)
-        if g is not None:
-            print(f'El ganador es {g}')
-            if g == 'X':
-                X["G"] += 1
-                O["P"] += 1
-            elif g == 'O':
-                O["G"] += 1
-                X["P"] += 1
-            else:
-                X["E"] += 1
-                O["E"] += 1
-        else:
-            print('Empate')
-            X["E"] += 1
-            O["E"] += 1
         tablero.actualiza_score(score,g)
 
 if __name__ == '__main__':
