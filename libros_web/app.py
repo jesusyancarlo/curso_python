588f27a5683bfbe78bf55202f07085351d127d46
jesusyancarlo <jesusyancarlo2005@gmail.com>
2025-04-04 00:08:59 -0700

    Búsqueda por libro y por letra en web ya son funcionales

--- libros_web/app.py
+++ libros_web/app.py
@@ -10,6 +10,7 @@ diccionario_id = fn.crea_diccionario(lista_libros,'id')
 diccionario_titulos = fn.crea_diccionario(lista_libros,'title')
 diccionario_autores = fn.crea_diccionario(lista_libros,'author')
 
+
 @app.route('/')
 def inicio():
     ''' Página de inicio de la aplicación '''
@@ -26,14 +27,35 @@ def busqueda_titulo():
         print(resultado)
     return render_template('titulo.html', lista_libros=resultado)
 
-@app.route('/libro/<id>', methods =['GET'])
-def libro(id:str):
+@app.route('/libro/<id_libro>', methods =['GET'])
+def libro(id_libro:str):
     ''' Página de información de un libro '''
-    if id in diccionario_id:
-        libro = diccionario_id[id]
-        return render_template('libro.html', libro=libro)
+    if id_libro in diccionario_id:
+        book = diccionario_id[id_libro]
+        return render_template('libro.html', libro=book)
     else:
         return render_template('libro.html', libro=None)
+
+@app.route('/letra/', methods =['GET'])
+def plantilla_letra():
+    ''' Página de búsqueda por letra '''
+    return render_template('letra.html', lista_libros=[])
+
+@app.route('/letra/<letra>', methods =['GET'])
+def busqueda_letra(letra:str):
+    ''' Página de búsqueda por letra '''
+    resultado = fn.libros_empiezan_con(lista_libros, letra)
+    return render_template('letra.html', lista_libros=resultado)
+
+@app.route('/title',  methods =['GET','POST'])
+def title():
+    ''' Página de búsqueda por título '''
+    print(request.method)
+    resultado = []
+    if request.method == 'POST':
+        titulo = request.form.get('searchInput', '')
+        resultado = fn.busca_en_diccionario(diccionario_titulos, titulo)
+    return render_template('titulos.html', lista_libros=resultado)
 
 if __name__ == '__main__':
     app.run(debug=True)
