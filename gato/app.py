02c87e4b45706f2c61f082900cc1449689d650a8
jesusyancarlo <jesusyancarlo2005@gmail.com>
2025-04-03 19:01:10 -0700

    Ciclo principal de app con actualización de score

--- gato/app.py
+++ gato/app.py
@@ -5,13 +5,29 @@ import tablero
 
 def main():
     ''' Función principal '''
-    numeros = [str(i) for i in range(1, 10)]
-    dsimbolos = {x: x for x in numeros}
-    g = tablero.juego(dsimbolos)
-    if g is not None:
-        print(f'El ganador es {g}')
-    else:
-        print('Empate')
+    X = {"G":0,"P":0,"E":0}
+    O = {"G":0,"P":0,"E":0}
+    score = {"X":X,"O":O}
+    numeros =[str(i) for i in range(1,10)]
+    corriendo = True
+    while corriendo:
+        dsimbolos = {x:x for x in numeros}
+        g = tablero.juego(dsimbolos)
+        if g is not None:
+            print(f'El ganador es {g}')
+            if g == 'X':
+                X["G"] += 1
+                O["P"] += 1
+            elif g == 'O':
+                O["G"] += 1
+                X["P"] += 1
+            else:
+                X["E"] += 1
+                O["E"] += 1
+        else:
+            print('Empate')
+            X["E"] += 1
+            O["E"] += 1
 
 if __name__ == '__main__':
     main()
