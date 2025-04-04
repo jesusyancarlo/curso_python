a93f9292d0c8ae77f0b180b06e0a0ba0e8e62bd1
jesusyancarlo <jesusyancarlo2005@gmail.com>
2025-04-03 20:21:51 -0700

    La clase Game ya genera JSON correctamente

--- games/Game.py
+++ games/Game.py
@@ -3,6 +3,7 @@ from random import choice
 from Athlete import Athlete
 from Sport import Sport
 from Team import Team
+import json
 
 class Game:
     ''' Clase Game: Juego entre dos equipos'''
@@ -11,7 +12,8 @@ class Game:
             'NBA': [x for x in range(50,136)],
             'NFL': [x for x in range(0,61)],
             'MLB': [x for x in range(0,21)],
-            'MLX': [x for x in range(0,11)]
+            'MLX': [x for x in range(0,11)],
+            'FIFA': [x for x in range(0,11)]
         }
     def __init__(self, A:Team, B:Team):
         ''' Constructor de la clase Game '''
@@ -58,6 +60,6 @@ if __name__ == "__main__":
     print(repr(game))
     print(game.to_json())
     filename_json = "game.json"
-    with open(filename_json, "w",encoding='utf8') as f:
-        f.write(str(game.to_json()))
+    with open(filename_json, "w", encoding='utf8') as f:
+     json.dump(game.to_json(), f, ensure_ascii=False, indent=4)
     print(f"Archivo {filename_json} guardado con éxito!")
